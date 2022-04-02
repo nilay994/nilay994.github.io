fc6928118542f701b8ec75ca97f2b1edabc7953c
Nilay Sheth <nilay_994@hotmail.com>
2022-04-02 15:29:21 +0200

    [carousel] try with npm loading fails

--- js/splide_carousel.js
+++ js/splide_carousel.js
@@ -1,12 +1,29 @@
+// Doesn't load using npm: import Splide from '@splidejs/splide';
+// Doesn't wait on loading the document either :(
+// document.addEventListener( 'DOMContentLoaded', function() {
+
+
 const Splide = window.Splide;
 
-var main = new Splide( '#archived-projects-carousal', {updateOnMove: true,
-                                                       type        : 'loop',
-                                                       perPage     : 3,
-                                                       perMove     : 1,
-                                                       focus       : 'center',
-                                                       width       : '90%',
-                                                       autoWidth   : true,
-                                                       lazyLoad    : true}
-                    );
-main.mount()
+var splide_proj_6 = new Splide( 
+    '#splide_proj_6', 
+    {
+        updateOnMove: true,
+        type        : 'loop',
+        perPage     : 3,
+        perMove     : 1,
+        focus       : 'center',
+        width       : '90%',
+        autoWidth   : true,
+        lazyLoad    : true,
+    }
+);
+
+splide_proj_6.mount();
+
+
+// creates styling problems
+//var elms = document.getElementsByClassName( 'splide' );
+// for ( var i = 0; i < elms.length; i++ ) {
+    // new Splide( elms[ i ] ).mount();
+//}
